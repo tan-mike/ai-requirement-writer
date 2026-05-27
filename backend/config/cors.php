6c105c6e92cb33e9ae99337afa6532dd2cab3a70
Michael Tan <michaeltan.socoe@gmail.com>
2026-05-27 22:20:05 +0800

    chore: use relative API paths for unified deployment

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3030')],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
